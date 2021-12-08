077fafa3d82e964bc4996dd2f4e2c6e07f3b4f2b
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2021-12-08 23:30:39 +0100

    init spring boot

---
+++ src/main/kotlin/ch/ruefenacht/sandro/animewebwatchlist/AnimeWebWatchlistApplication.kt
@@ -0,0 +1,11 @@
+package ch.ruefenacht.sandro.animewebwatchlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnimeWebWatchlistApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnimeWebWatchlistApplication>(*args)
+}
---
+++ src/test/kotlin/ch/ruefenacht/sandro/animewebwatchlist/AnimeWebWatchlistApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.ruefenacht.sandro.animewebwatchlist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnimeWebWatchlistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
